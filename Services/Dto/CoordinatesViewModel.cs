71fb0943aec8d0b775f0b222e1c50a973e0b2d32
José Girón <42883411+Jocagi@users.noreply.github.com>
2021-10-12 18:58:57 -0600

    Coordenadas con API de Google Maps

---
+++ Services/Dto/CoordinatesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MisVacaciones.Services.Dto
+{
+    public class CoordinatesViewModel
+    {
+        public double longitude { get; set; }
+        public double latitude { get; set; }
+    }
+}
